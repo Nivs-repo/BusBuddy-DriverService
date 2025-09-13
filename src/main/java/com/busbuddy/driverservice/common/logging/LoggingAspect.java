6fbf93e1866c7e902077d03e30135d04379102b8
Nivetha Suresh <mail.nivethasuresh@gmail.com>
2025-09-13 18:20:33 +0530

    Application insights config & Logging aspect update

--- src/main/java/com/busbuddy/driverservice/common/logging/LoggingAspect.java
+++ src/main/java/com/busbuddy/driverservice/common/logging/LoggingAspect.java
@@ -11,30 +11,74 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
+    // ----------- CONTROLLER -----------
     @Before("execution(* com.busbuddy.driverservice..controller..*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        MethodSignature method = (MethodSignature) joinPoint.getSignature();
-        log.info(" Incoming request: {}.{} args={}",
-                method.getDeclaringTypeName(),
-                method.getName(),
-                joinPoint.getArgs());
+    public void logControllerBefore(JoinPoint joinPoint) {
+        logMethodEntry(joinPoint, "Controller");
     }
 
     @AfterReturning(value = "execution(* com.busbuddy.driverservice..controller..*(..))", returning = "response")
-    public void logAfterReturning(JoinPoint joinPoint, Object response) {
-        MethodSignature method = (MethodSignature) joinPoint.getSignature();
-        log.info(" Response from {}.{}: {}",
-                method.getDeclaringTypeName(),
-                method.getName(),
-                response);
+    public void logControllerAfterReturning(JoinPoint joinPoint, Object response) {
+        logMethodExit(joinPoint, "Controller", response);
     }
 
+    // ----------- SERVICE -----------
+    @Before("execution(* com.busbuddy.driverservice..service..*(..))")
+    public void logServiceBefore(JoinPoint joinPoint) {
+        logMethodEntry(joinPoint, "Service");
+    }
+
+    @AfterReturning(value = "execution(* com.busbuddy.driverservice..service..*(..))", returning = "response")
+    public void logServiceAfterReturning(JoinPoint joinPoint, Object response) {
+        logMethodExit(joinPoint, "Service", response);
+    }
+
+    // ----------- REPOSITORY -----------
+    @Before("execution(* com.busbuddy.driverservice..repository..*(..))")
+    public void logRepositoryBefore(JoinPoint joinPoint) {
+        logMethodEntry(joinPoint, "Repository");
+    }
+
+    @AfterReturning(value = "execution(* com.busbuddy.driverservice..repository..*(..))", returning = "response")
+    public void logRepositoryAfterReturning(JoinPoint joinPoint, Object response) {
+        logMethodExit(joinPoint, "Repository", response);
+    }
+
+    // ----------- EXCEPTIONS (all layers) -----------
     @AfterThrowing(value = "execution(* com.busbuddy.driverservice..*(..))", throwing = "ex")
     public void logException(JoinPoint joinPoint, Exception ex) {
         MethodSignature method = (MethodSignature) joinPoint.getSignature();
-        log.error(" Exception in {}.{}: {}",
-                method.getDeclaringTypeName(),
-                method.getName(),
+        log.error("❌ Exception in {}.{}: {}", 
+                method.getDeclaringTypeName(), 
+                method.getName(), 
                 ex.getMessage(), ex);
+    }
+
+    // ----------- Helpers -----------
+    private void logMethodEntry(JoinPoint joinPoint, String layer) {
+        MethodSignature method = (MethodSignature) joinPoint.getSignature();
+        log.info("➡ [{}] Entering {}.{} args={}", 
+                layer, 
+                method.getDeclaringTypeName(), 
+                method.getName(), 
+                joinPoint.getArgs());
+    }
+
+    private void logMethodExit(JoinPoint joinPoint, String layer, Object response) {
+        MethodSignature method = (MethodSignature) joinPoint.getSignature();
+        // For repositories, avoid logging full list/objects
+        if ("Repository".equals(layer) && response instanceof java.util.Collection) {
+            log.info("⬅ [{}] Exiting {}.{} returned {} records", 
+                    layer, 
+                    method.getDeclaringTypeName(), 
+                    method.getName(), 
+                    ((java.util.Collection<?>) response).size());
+        } else {
+            log.info("⬅ [{}] Exiting {}.{} response={}", 
+                    layer, 
+                    method.getDeclaringTypeName(), 
+                    method.getName(), 
+                    response);
+        }
     }
 }
